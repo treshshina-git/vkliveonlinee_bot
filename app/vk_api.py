a2dafbc1e82a799833f61ccd54dfe85a5d191d0b
treshshina-git <treshshina@gmail.com>
2026-06-15 21:39:02 +0000

    1

--- app/vk_api.py
+++ app/vk_api.py
@@ -2,24 +2,23 @@ import requests
 from app.token_manager import get_access_token
 from app.config import CHAT_RULETTE_CATEGORY_ID, API_URL
 API_URL1 = "https://apidev.live.vkvideo.ru/v1/catalog/online_categories"
+print("API_URL1 - ", API_URL1)
 def get_online_streams():
     token = get_access_token()
     r = requests.get(
-        API_URL1,
+        API_URL,
         headers={"Authorization": f"Bearer {token}"},
         params={
             "limit": 200,
             "offset": 0,
-            "category_type": ""
+            "category_id": CHAT_RULETTE_CATEGORY_ID,
+            "all_streams": True,
+            "has_vk_video": True
         },
         timeout=30
     )
-
     r.raise_for_status()
     data = r.json()
-    print("data - ", data)
-
-
     streams = []
     for item in data.get("data", {}).get("channels", []):
         stream = item.get("stream", {})
