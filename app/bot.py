a1df0791c08a1065e371dde52219b587124d6e00
treshshina-git <treshshina@gmail.com>
2026-06-15 19:20:32 +0000

    1

--- app/bot.py
+++ app/bot.py
@@ -19,7 +19,7 @@ def build_keyboard():
         ]
     ])
 def format_streams(streams):
-    return "⚞ⓉⓇⒺⓈⒽⒾⓃⒶ⚟ ⛭ ▁▂▅▇ ⟸ ҬҎЕѰЍҢӐ ⟹\n".join(
+    return "▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n".join(
         f"👤 {s['owner']} \n"
         f"🔴 {s['title']} \n"
         f"👁 {s['viewers']} - {s['url']} \n"
