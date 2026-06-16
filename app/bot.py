e149503d13f881b9aadd4da140a3e55ae649b090
treshshina-git <treshshina@gmail.com>
2026-06-16 00:32:35 +0000

    3

--- app/bot.py
+++ app/bot.py
@@ -22,10 +22,10 @@ def build_keyboard():
     ])
 
 def format_streams(streams):
-    return "🔘🔘🔘🔘🔘🔘🔘🔘🔘🔘🔘🔘🔘🔘🔘\n".join(
-        f"🎤 <b>{s['owner']}</b> - <a href='{s['url']}'>Ссылка</a>\n"
-        f"𝄩 \"{s['title']}\"\n"
-        f"👓 {s['viewers']}\n"
+    return "▼🔘▼🔘▼🔘▼🔘▼🔘▼🔘▼🔘▼🔘▼🔘▼\n\n".join(
+        f"📢 <b>{s['owner']}</b> - <a href='{s['url']}'>Ссылка</a>\n"
+        f"➡ \"{s['title']}\"\n"
+        f"👓 {s['viewers']}\n\n"
         for s in streams
     )
 async def send(update, context, mode="all"):
