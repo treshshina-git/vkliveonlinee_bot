db8d8636e21e40828cdfda88ab203f9efc17b63d
treshshina-git <treshshina@gmail.com>
2026-06-16 04:10:45 +0000

    1

--- app/bot.py
+++ app/bot.py
@@ -37,12 +37,16 @@ def format_streams(streams):
 async def sendsec(update, context, mode="all"):
     sections = get_online_sections()
     #print("Sections received from VK API - ", sections)
-    sections.sort(key=lambda x: x["viewers"], reverse=True)
     for sec in sections:
         if len(sec["name"]) > 30:
             sec["name"] = sec["name"][:30] + "..."
+            sections[sections.index(sec)]["name"] = sec["name"]
         else:
             sec["name"] = sec["name"]
+            sections[sections.index(sec)]["name"] = sec["name"]
+    
+    sections.sort(key=lambda x: x["viewers"], reverse=True)
+
     text = format_sections(sections)
     print(text)
  
