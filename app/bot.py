d1eb786bfd71384f76baf27fc2d8558e50db790d
treshshina-git <treshshina@gmail.com>
2026-06-16 05:05:33 +0000

    1

--- app/bot.py
+++ app/bot.py
@@ -60,7 +60,7 @@ async def sendsec(update, context, mode="all"):
     )
 
 async def send(update, context, mode="all"):
-    section_id = context.user_data.get("section_id")
+    #section_id = context.user_data.get("section_id")
     print(f"Section ID: {section_id}")
     streams = get_online_streams()
     streams.sort(key=lambda x: x["viewers"], reverse=True)
