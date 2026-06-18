f40ad13184ec4aa7ab09e7407b5e3901ea61445a
treshshina-git <treshshina@gmail.com>
2026-06-18 08:58:56 +0000

    545

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ async def show_channels_for_category(query, context: ContextTypes.DEFAULT_TYPE, 
         webapp_url = f"https://vkliveonline.up.railway.app/player"
         #print(webapp_url)
 
-        namer = f"▶ {name:<30} - {view:.^15}"
+        namer = f"▶ {name} -        {view}"
         current_row.append(
             InlineKeyboardButton(
                 text = namer
