3def43140007ec957ad812198fa1483aaed43057
treshshina-git <treshshina@gmail.com>
2026-06-18 08:26:08 +0000

    333

--- main.py
+++ main.py
@@ -308,11 +308,11 @@ async def show_channels_for_category(query, context: ContextTypes.DEFAULT_TYPE, 
         #print(encoded_urik)
         #webapp_url = f"{WEBAPP_BASE_URL}/?play=1&urik={urik}"
         webapp_url = f"https://vkliveonlineebot-production.up.railway.app/player"
-        print(webapp_url)
+        #print(webapp_url)
         namer = f"{name} - {view}"
         current_row.append(
             InlineKeyboardButton(
-                "▶ Смотреть стрим",
+                "▶ {namer}",
                 web_app={"url": webapp_url}
                 #text=namer, web_app={"url": webapp_url}
             )
