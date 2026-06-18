8cfbb08f8dfed4cd3d672d82a45b925caa9ba774
treshshina-git <treshshina@gmail.com>
2026-06-18 09:14:06 +0000

    4444

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import os, base64
 from typing import Any, Dict, List, Optional
-from fastapi import FastAPI, Requests
+from fastapi import FastAPI, requests
 from fastapi.staticfiles import StaticFiles
 import httpx
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
