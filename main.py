a42122ab41f40f0bd1f5549dd97054f7fd4b05df
treshshina-git <treshshina@gmail.com>
2026-06-18 09:35:28 +0000

    8888

--- main.py
+++ main.py
@@ -353,7 +353,7 @@ async def show_channels_for_category(query, context: ContextTypes.DEFAULT_TYPE, 
 async def openn(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
  
 
-    url = "https://api.telegram.org/bottoken/getFile"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/getFile"
 
     payload = { "file_id": "Required" }
     headers = {
